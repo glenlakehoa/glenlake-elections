7b26f47c4706ffe81b83a99e54a30c9caef81383
Rob Hanssen <rob.hanssen@outlook.com>
2022-10-06 16:45:40 -0400

    predications and diagnostics

--- dev/vote-prediction.r
+++ dev/vote-prediction.r
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(lubridate)
+library(patchwork)
 theme_set(theme_light())
 
 load("Rdata/votes.Rdata")
@@ -39,3 +40,42 @@ predictions %>%
     coord_cartesian(ylim = c(0, 180))
 
 ggsave("graphs/vote-predictions.png", width = 6, height = 6)
+
+
+
+votemodeldata <-
+    votemodels %>%
+    mutate(dfs = map(data, ~ nrow(.x) - 2)) %>%
+    unnest(dfs) %>%
+    mutate(qt = qt(pnorm(1), df = dfs)) %>%
+    mutate(params = map(linmod, broom::tidy)) %>%
+    unnest(params) %>%
+    mutate(
+        estimate = abs(estimate),
+        .lower = estimate - qt * std.error,
+        .upper = estimate + qt * std.error
+    ) %>%
+    select(-data, -linmod, -statistic, -p.value, -dfs, -std.error, -qt) %>%
+    pivot_wider(
+        names_from = "term",
+        names_sep = "_",
+        values_from = c(estimate, .upper, .lower)
+    ) %>%
+    janitor::clean_names()
+
+(votemodeldata %>%
+    ggplot() +
+    aes(year, estimate_intercept) +
+    geom_point() +
+    geom_errorbar(aes(ymin = lower_intercept, ymax = upper_intercept)) +
+    geom_hline(yintercept = 120, color = "red") +
+    labs(x = "Year", y = "Estimated votes")) +
+(votemodeldata %>%
+    ggplot() +
+    aes(year, estimate_daysuntilelection) +
+    geom_point() +
+    geom_errorbar(aes(ymin = lower_daysuntilelection, ymax = upper_daysuntilelection)) +
+    geom_hline(yintercept = 120 / 30, color = "red") +
+    labs(x = "Year", y = "Voting rate"))
+
+ggsave("graphs/vote-diagnostics.png", width = 8, height = 4)
