fd3a4825415bef682750c170ff6c384c395490f6
Rob Hanssen <rob.hanssen@outlook.com>
2022-10-07 10:55:45 -0400

    quorum data model

--- dev/vote-prediction.r
+++ dev/vote-prediction.r
@@ -61,19 +61,50 @@ votemodeldata <-
         names_sep = "_",
         values_from = c(estimate, .upper, .lower)
     ) %>%
-    janitor::clean_names()
+    janitor::clean_names() %>%
+    ungroup()
 
 max_votes <-
     votes %>%
     group_by(year) %>%
     summarise(votes = max(votesreceived), .groups = "drop")
 
+quorum_filter <- votes %>%
+    group_by(year) %>%
+    filter(voteneeded < 0) %>%
+    distinct(year) %>%
+    pull(year)
+
+quorum_reached <-
+    votes %>%
+    filter(year %in% quorum_filter) %>%
+    group_by(year) %>%
+    nest() %>%
+    mutate(q_reach = map_dbl(
+        data,
+        ~ with(
+            .x,
+            approx(voteneeded, daysuntilelection, 0, na.rm = TRUE)
+        )$y
+    )) %>%
+    ungroup() %>%
+    select(year, q_reach)
+
+
+quorum_model <- votemodeldata %>%
+    mutate(
+        q_reach = (estimate_intercept - 120) / estimate_daysuntilelection,
+        q_high = (upper_intercept - 120) / lower_daysuntilelection,
+        q_low = (lower_intercept - 120) / upper_daysuntilelection
+    ) %>%
+    select(starts_with("q"), year)
+
 
 (votemodeldata %>%
     ggplot() +
     aes(year, estimate_intercept) +
     geom_point() +
-    geom_point(data = max_votes, aes(y = votes), shape = 10, size = 4) + 
+    geom_point(data = max_votes, aes(y = votes), shape = 10, size = 4) +
     geom_errorbar(aes(ymin = lower_intercept, ymax = upper_intercept)) +
     geom_hline(yintercept = 120, color = "red") +
     labs(x = "Year", y = "Estimated votes")) +
@@ -81,8 +112,19 @@ max_votes <-
     ggplot() +
     aes(year, estimate_daysuntilelection) +
     geom_point() +
-    geom_errorbar(aes(ymin = lower_daysuntilelection, ymax = upper_daysuntilelection)) +
+    geom_errorbar(aes(ymin = lower_daysuntilelection,
+                      ymax = upper_daysuntilelection)) +
     geom_hline(yintercept = 120 / 30, color = "red") +
-    labs(x = "Year", y = "Voting rate"))
+    labs(x = "Year", y = "Voting rate")) /
+(quorum_model %>%
+    ggplot() +
+    aes(year, q_reach) +
+    geom_point() +
+    geom_errorbar(aes(ymin = q_low, ymax = q_high)) +
+    geom_point(data = quorum_reached, shape = 10, size = 4) +
+    scale_y_continuous(limits = c(0, NA)) +
+    geom_hline(yintercept = 0, color = "red") +
+    labs(x = "Year", y = "Quorum reached (in days before election)")
+)
 
-ggsave("graphs/vote-diagnostics.png", width = 8, height = 4)
+ggsave("graphs/vote-diagnostics.png", width = 8, height = 8)
