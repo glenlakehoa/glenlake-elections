6177893816a8fb494fdc0edcf881a792f3269d2a
Rob Hanssen <rob.hanssen@outlook.com>
2022-10-13 09:23:48 -0400

    using unnest_wider to unlist json columns

--- dev/import.r
+++ dev/import.r
@@ -6,11 +6,9 @@ source_files <- list.files("sources/", pattern = "*.json", full.names = TRUE)
 
 votes <-
     map_dfr(source_files, 
-            ~jsonlite::read_json(.x, simplifyDataFrame = FALSE)) %>% 
-    mutate(date = sapply(votes, function(x) x[[1]]),
-           votesreceived = sapply(votes, function(x) x[[2]]),
-           votes = NULL
-            ) %>%
+            ~jsonlite::read_json(.x)) %>%
+    unnest_wider(votes) %>%
+    rename(votesreceived = votes) %>%
     mutate(
         across(ends_with("date"), lubridate::ymd),
         quorum = numberofhomes %/% 4,
